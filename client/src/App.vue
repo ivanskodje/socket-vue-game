cb0248d47562f67af8e3faa33ed312e4ecf7ada7
Ivan Skodje <ivanskodje@protonmail.com>
2021-09-12 16:45:48 +0200

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Game />
+  </div>
+</template>
+
+<script>
+import Game from "./components/Game.vue";
+
+export default {
+  name: "App",
+  components: {
+    Game,
+  },
+};
+</script>
+
+<style></style>
